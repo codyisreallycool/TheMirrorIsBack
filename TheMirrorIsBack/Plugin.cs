fc68c3c9ea7e249bf05ce348af1775feb7171825
xoJKox <xoJKox@B-PC>
2023-08-20 09:10:43 +1200

    Add project files.

---
+++ TheMirrorIsBack/Plugin.cs
@@ -0,0 +1,96 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace TheMirrorIsBack
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        GameObject mirror;
+
+        void Start()
+        {
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            FindAndActivateMirror();
+        }
+
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+            DeactivateMirror();
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            Transform parentTransform = GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/TreeRoomInteractables").transform;
+            mirror = FindGameObjectWithNameContains(parentTransform, "mirror");
+            if (mirror != null)
+            {
+                Logger.LogInfo("Found mirror GameObject! :D");
+                mirror.SetActive(true);
+            }
+            else
+            {
+                Logger.LogWarning("Mirror GameObject not found D:");
+            }
+        }
+
+        void Update()
+        {
+        }
+
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+        }
+
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            inRoom = false;
+        }
+
+        void FindAndActivateMirror()
+        {
+            if (mirror != null)
+            {
+                mirror.SetActive(true);
+            }
+            else
+            {
+                Logger.LogWarning("Mirror GameObject not found D:");
+            }
+        }
+
+        void DeactivateMirror()
+        {
+            if (mirror != null)
+            {
+                mirror.SetActive(false);
+            }
+        }
+
+        GameObject FindGameObjectWithNameContains(Transform parent, string substring)
+        {
+            foreach (Transform child in parent)
+            {
+                if (child.name.Contains(substring))
+                {
+                    return child.gameObject;
+                }
+            }
+            return null;
+        }
+    }
+}
